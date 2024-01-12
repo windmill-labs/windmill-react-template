e442ca2c7e79cebba46996d1c30a6609cb0b43e8
Kieran Mann <kieran.mann@o8t.com>
2024-01-12 15:35:06 -0800

    read app code and align with react best practice, implementing what seems to have been started with the whoami function

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,51 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import testScript from "../scripts/test.ts?raw";
 import windmillLogo from "./assets/logo.svg";
-import { OpenAPI, UserService } from "windmill-client";
-import { email, executeInlineScript, username } from "./utils";
-
-async function whoami() {
-  const res = await UserService.globalWhoami();
-  console.log(res);
-}
+import { GlobalUserInfo, OpenAPI, Preview, UserService } from "windmill-client";
+import { executeInlineScript } from "./utils";
 
 function App() {
   const [result, setResult] = useState("");
   const [arg, setArg] = useState("");
   const [loading, setLoading] = useState(false);
 
-  async function executeTest() {
+  const [userDetails, setUserDetails] = useState<GlobalUserInfo>();
+
+  const executeTest = useCallback(async () => {
     setLoading(true);
-    const result = await executeInlineScript("deno", testScript, {
-      x: arg,
-    });
+    const result = await executeInlineScript(
+      Preview.language.DENO,
+      testScript,
+      {
+        x: arg,
+      }
+    );
     setLoading(false);
     setResult(result);
-  }
+  }, [arg]);
+
+  const populateUser = useCallback(async () => {
+    const userDetails = await UserService.globalWhoami();
+    setUserDetails(userDetails);
+  }, []);
 
   useEffect(() => {
     if (import.meta.env.MODE === "development") {
       console.log("Running in development mode");
       OpenAPI.TOKEN = import.meta.env.VITE_TOKEN;
     }
-    whoami();
-    executeTest().then();
-  }, []);
+    populateUser();
+    executeTest();
+  }, [executeTest, populateUser]);
 
   return (
     <>
       <img src={windmillLogo} className="logo windmill" alt="Windmill logo" />
 
       <h1 className="my-2">Windmill React App</h1>
-      <div>Username: {username}</div>
-      <div>Email: {email}</div>
+      <div>Name: {userDetails?.name}</div>
+      <div>Email: {userDetails?.email}</div>
 
       <pre className="text-left border p-2 bg-gray-50 mt-4">
         <code>{testScript}</code>
